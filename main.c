ce11a0843f48441d5b225570335c331cdf0b7c60
JoaoSpezia <spezia548@gmail.com>
2024-04-18 12:24:46 -0300

    Validação String Att

--- main.c
+++ main.c
@@ -9,21 +9,29 @@ int intervalo( char c );
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL,"Portuguese");
 	
-	char *temp = malloc( 50 );
-	printf("\n Insira a Expressão: ");
-	gets( temp );
-	int tam_str = strlen(temp);
-	
-	int i, cont = 0, valid = 1;
-	char *temp2 = malloc( tam_str );
-	for(i=0; temp[i]!='\0' && valid; i++) {
-		if( temp[i]!=32 ) {
-			temp2[cont] = temp[i];
-			if( !intervalo( temp2[cont++]) ) {
-				valid = 0;
+	char *temp = malloc( 50 ), *temp2;
+	int tam_str, i, cont = 0, valid = 1;
+	do {
+		if(!valid) {
+			system("CLS");
+			printf("\n Valores Inválidos!");
+		}
+		valid = 1;
+		printf("\n Insira a Expressão: ");
+		gets( temp );
+		tam_str = strlen(temp);
+		temp2 = malloc( tam_str );
+		for(i=0; temp[i]!='\0' && valid; i++) {
+			if( temp[i]!=32 ) {
+				if( !intervalo( temp[i]) ) {
+					valid = 0;
+				}
+				if( valid ) {
+					temp2[cont++] = temp[i];
+				}			
 			}
 		}
-	}
+	} while( !valid );
 	
 	free( temp );
 	char E[tam_str];
@@ -37,7 +45,7 @@ int main(int argc, char *argv[]) {
 }
 
 int intervalo( char c ) {
-	if( c<40 || ( c>43 && c<45 ) || ( c>45 && c<47 ) || c>57  ) {
+	if( c<40 || c==44 || c==46 || c>57  ) {
 		return 0;
 	}
 	return 1;
